2e150587db6fff0d13d5f6e25b0583894f222bd4
lilamb <129888031+lilamb@users.noreply.github.com>
2023-04-24 13:58:47 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// rock-paper-sccissors
+// An implementation of the game
+  //Aak player if they would like to play a game of rock-paper-sccissors
+  // Start the game loop
+  //Prompt them for a input of rock, paper, or sccissors
+    //0=rock, 1=paper, 2=sccissors
+  //Compare the user inpu to the randomly generated 
+  //Test the or draw conditions 
+  //Ask player if they would like to play again. 
+    //If yes restart the game loop
+    //If no end game loop  
+// Leeza Lamb <llamb@UCSC.edu>
+// April 2023
