a5d1f7db145b8a39ec0acd3f68185893ebcc98cb
lilamb <129888031+lilamb@users.noreply.github.com>
2023-04-26 13:31:02 -0700

    Lab 5 finish

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,13 +1,13 @@
+// Leeza Lamb <llamb@UCSC.edu>
+// April 2023
 // rock-paper-sccissors
 // An implementation of the game
   //Aak player if they would like to play a game of rock-paper-sccissors
   // Start the game loop
   //Prompt them for a input of rock, paper, or sccissors
     //0=rock, 1=paper, 2=sccissors
-  //Compare the user inpu to the randomly generated 
+  //Compare the user input to the randomly generated 
   //Test the or draw conditions 
   //Ask player if they would like to play again. 
     //If yes restart the game loop
     //If no end game loop  
-// Leeza Lamb <llamb@UCSC.edu>
-// April 2023
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Thunderbird 
+// Author: Leeza Lamb
+// Date: 4/25/2023
 
-// Constants
+  //variables 
+    make = "Ford"; 
+    model = "Thunderbird"; 
+    color = "Rusty Red"
+    year = "1960";
 
-// Functions
+  //calculate 
+    age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Note, that that is WRITELN with an "L" not WRITEIN
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: "+ model+ "<br>");
+document.writeln("Color: "+ color+ "<br>");
+document.writeln("Year: "+ year+ "<br>");
+document.writeln("Age: "+ age+ "<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
