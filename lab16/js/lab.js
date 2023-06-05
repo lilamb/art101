a9addb333d2a5dafa4de757ceac4d59441ee6ee4
lilamb <129888031+lilamb@users.noreply.github.com>
2023-06-05 16:10:01 -0700

    Debuging

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,24 +1,23 @@
 // index.js - Ajax and Json 
 // Author: Leeza Lamb
-// Date: 6/1/2023
+// Date: 6/5/2023
 
 $.ajax({
-    //URL
+    //url
     url: "https://xkcd.com/info.0.json",
-    //Want all of the data for the most recent comic
-    //Image the API has updated it will change into
+    //want all of the data for most recent comic on api 
+    //whatver image the api has updated, it will change into it
     data: {},
     type: "GET",
     dataType: "json",
-    success: function(data) {
-        //callback function (code does with the retrieved data)
+    success: function(data){
+        //success callback function (what code does with the retrieved data)
         console.log("success");
         console.log(data);
         var comicObj = data;
-        //put the title on the page
+        //puts title on page
         $("#output").append("<h3>" + comicObj.title + "</h3>");
-        //put the image on the pafe and attaches alt text and title to image 
-        $("#output").append("<img src=" + comicObj.img + "alt=" + comicObj.alt + "title='Image'>");
+        //puts image on page, and attaches alt text & title to image
+        $("#output").append("<img src=" + comicObj.img + " alt=" + comicObj.alt + "title='Image'>");
     }
-
 })
